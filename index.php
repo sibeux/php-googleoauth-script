083530533675161c7a803eb5f42595b918bbf554
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-08-24 23:29:49 +0700

    a

--- index.php
+++ index.php
@@ -7,20 +7,26 @@ $client = new Google_Client();
 // Arahkan client untuk menggunakan file kunci Service Account
 $client->setAuthConfig('./credentials/wahabinasrul_credential.json');
 
-// Tentukan scope (izin) yang dibutuhkan oleh aplikasi Anda
+// --- LANGKAH B: TENTUKAN IZIN (SCOPE) YANG DIBUTUHKAN ---
+// Pastikan scope ini sesuai dengan API yang akan Anda panggil
 $client->setScopes([
-    'https://www.googleapis.com/auth/drive.readonly',
+    'https://www.googleapis.com/auth/drive.readonly'
 ]);
 
-// Sekarang client Anda sudah terotentikasi!
-// Anda bisa langsung menggunakannya untuk mengakses layanan.
+// Sampai di sini, client sudah memiliki identitas.
+// Sekarang Anda bisa membuat objek service.
 
-// Contoh: Mengakses Google Drive API
+// --- LANGKAH C: BUAT OBJEK SERVICE ---
 $driveService = new Google_Service_Drive($client);
 
-// Lakukan operasi API yang Anda inginkan...
-$files = $driveService->files->listFiles();
+// --- LANGKAH D: BARU LAKUKAN PANGGILAN API ---
+// Jangan lakukan ini sebelum langkah A dan B selesai.
+$files = $driveService->files->listFiles([
+    'pageSize' => 10,
+    'fields' => 'nextPageToken, files(id, name)'
+]);
 
+// Tampilkan hasilnya
 foreach ($files->getFiles() as $file) {
-    printf("Found file: %s (%s)\n", $file->getName(), $file->getId());
+    printf("File ditemukan: %s (%s)\n", $file->getName(), $file->getId());
 }
